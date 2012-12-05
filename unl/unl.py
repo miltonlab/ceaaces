a03b01328c9a9caecfb4dbe2e8f5f2f69bf7a3cc
miltonlab <1000ton.lab@gmail.com>
2012-12-05 11:27:40 -0500

    trabajando en filtros por carrera e inicio de cohorte

--- unl/unl.py
+++ unl/unl.py
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+
 ##############################################################################
 #
 # Copyright (c) 2004 TINY SPRL. (http://tiny.be) All Rights Reserved.
@@ -51,14 +53,15 @@ class unl_estudiante(osv.osv):
         'dni': fields.char('Cedula', size=15, help='Cedula o Pasaprte de Identidad'),        
         'nombres': fields.char('Nombres',size=35, help='Nombres del Estudiante'),
         'apellidos': fields.char('Apellidos', size=35, help='Apellidos del Docente'),
-        #'ingreso': fields.date('Inicio de Estudios', help=u'Fecha en la que inició sus estudios'),
-        #'duracion_carreara': fields.integer(u'Duración en Años', help=u'Duración de la Carrera en años')
+        'ingreso': fields.date('Inicio de Estudios', help=u'Fecha en la que inició sus estudios'),
+        'duracion_carrera': fields.integer(u'Duración en Años', help=u'Duración de la Carrera en años'),
         'egresado': fields.date('Egresado', help='Fecha en la que egreso de la carrera'),
         'semestre_egreso': fields.integer('Semestre_Egreso', help='Semestre en el que egreso'),
         'graduado': fields.date('Gradudado', help="Fecha de graduacion"),
         'titulo': fields.char('Titulo', size=35, help='Titulo que obtuvo en la Carrera'),
         'carrera': fields.char('Carrera',size=150, help='Nombre de la Carrera'),
         'area': fields.selection(areas, 'Area', help='Area Academica Administrativa de la UNL')
+        'anio_ingreso' : fields.function (anio_ingreso, type = 'integer', string = u'Año de Ingreso'),
 #	'carrera_ids': fields.many2many(
 #            'unl.carrera',
 #            'estudiante_carrera_rel',
@@ -67,6 +70,14 @@ class unl_estudiante(osv.osv):
 #            'Carreras'
 #        )
     }
+
+    def anio_ingreso(self, cr, uid, ids, fields, arg, context=None):
+        records = self.browse(cr, uid, ids)
+        print "testing ...", records
+        r = records[0]
+        result = {}
+        result[r.id] = r.igreso.year
+        return result
 
     _sql_constraints = [
         ('dni_carrera_unique', 'unique(dni, carrera)', u'La Cédula y la Carrera deben ser únicos !'),
